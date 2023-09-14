05ab0370fe368f780388353bdf020979ab07fd9b
Januarius93 <marstyczen@gmail.com>
2023-09-14 15:20:59 +0200

    added main service module

---
+++ estestingportal/src/main/java/com/estesting/estestingportal/EstestingportalApplication.java
@@ -0,0 +1,13 @@
+package com.estesting.estestingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstestingportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstestingportalApplication.class, args);
+	}
+
+}
